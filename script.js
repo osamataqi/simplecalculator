e48ebcd359d45c5632aace3daa09f8026cd8cf54
[osamataqi] <[osamataqiedeen@gmail.com]>
2018-06-13 01:12:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function clickButton(button){
+    if (document.getElementById("inputoutputBox").innerHTML){
+        document.getElementById("inputoutputBox").innerHTML = document.getElementById("inputoutputBox").innerHTML + button;
+    }
+    else {
+        document.getElementById("inputoutputBox").innerHTML = button;
+    }
+}
+
+function calculate(){
+    var result = eval(document.getElementById("inputoutputBox").innerHTML);
+    document.getElementById("inputoutputBox").innerHTML = result;
+}
+
+function del(){
+    if (document.getElementById("inputoutputBox").innerHTML){
+        document.getElementById("inputoutputBox").innerHTML = document.getElementById("inputoutputBox").innerHTML.substring(0,document.getElementById("inputoutputBox").innerHTML.length - 2)
+    }
+}
+
+function clr(){
+    document.getElementById("inputoutputBox").innerHTML = "";
+}
